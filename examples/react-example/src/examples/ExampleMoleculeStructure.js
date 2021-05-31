2c64e94f4d43b3e5e87d45c51194b95ca79a5b26
michelml <michmoreau.l@gmail.com>
2021-05-30 20:34:16 -0400

    present moleculestructure at the beginning

---
+++ examples/react-example/src/examples/ExampleMoleculeStructure.js
@@ -0,0 +1,19 @@
+function ExampleMoleculeStructure() {
+  return (
+    <div id="component-example-svg" className="container">
+      <section class="hero">
+        <div class="hero-body">
+          <p class="title">MoleculeStructure - A Basic React Component</p>
+          <p class="subtitle">
+            The examples below will mostly use the React component{" "}
+            <code className="is-family-code">MoleculeStructure</code> to render
+            the examples. You can see the React implementation in the code
+            below.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ExampleMoleculeStructure;
--- examples/react-example/src/index.js
+++ examples/react-example/src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CodeExample from "./components/CodeExample/CodeExample";
+import ExampleMoleculeStructure from "./examples/ExampleMoleculeStructure";
 import ExampleSVG from "./examples/ExampleSvg";
 import ExampleCanvas from "./examples/ExampleSvg";
 /* eslint import/no-webpack-loader-syntax: off */
+import MoleculeStructureCode from "!!raw-loader!./components/MoleculeStructure/MoleculeStructure";
 import ExampleSVGCode from "!!raw-loader!./examples/ExampleSvg.js";
 import ExampleCanvasCode from "!!raw-loader!./examples/ExampleCanvas.js";
 import "./index.css";
+
+ReactDOM.render(
+  <CodeExample code={MoleculeStructureCode}>
+    <ExampleMoleculeStructure />
+  </CodeExample>,
+  document.getElementById("example-mol-structure")
+);
 
 ReactDOM.render(
   <CodeExample code={ExampleSVGCode}>
