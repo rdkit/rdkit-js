683c98500e863ad95a3db8bd6d9bb9173d97b791
michelml <michmoreau.l@gmail.com>
2021-05-30 22:33:12 -0400

    complete substruct example

--- examples/react-example/src/index.js
+++ examples/react-example/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CodeExample from "./components/CodeExample/CodeExample";
 import ExampleMoleculeStructure from "./examples/ExampleMoleculeStructure";
+import ExampleSubstructure from "./examples/ExampleSubstructure";
 import ExampleSVG from "./examples/ExampleSvg";
 import ExampleCanvas from "./examples/ExampleSvg";
 /* eslint import/no-webpack-loader-syntax: off */
 import MoleculeStructureCode from "!!raw-loader!./components/MoleculeStructure/MoleculeStructure";
 import ExampleSVGCode from "!!raw-loader!./examples/ExampleSvg.js";
 import ExampleCanvasCode from "!!raw-loader!./examples/ExampleCanvas.js";
+import ExampleSubstructureCode from "!!raw-loader!./examples/ExampleSubstructure.js";
 import "./index.css";
 
 ReactDOM.render(
@@ -29,4 +31,11 @@ ReactDOM.render(
     <ExampleCanvas />
   </CodeExample>,
   document.getElementById("example-canvas")
+);
+
+ReactDOM.render(
+  <CodeExample code={ExampleSubstructureCode}>
+    <ExampleSubstructure />
+  </CodeExample>,
+  document.getElementById("example-substructures")
 );
