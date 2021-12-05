a14319d97917b7277d81fccfc1dc5299e346b1d1
michelml <michmoreau.l@gmail.com>
2021-12-04 23:04:52 -0500

    remove react-redux examples to remove redundancy in code add side nav for vanillajs example add link from vanillajs to react examples

--- examples/react-redux-example/src/redux/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { rdKitStateReducer } from "./rdkit/reducers";
-
-export const rootReducer = combineReducers({
-  rdKitState: rdKitStateReducer,
-});
