c3671c39a10b7a7992a2900aa55926118498534c
michelml <michmoreau.l@gmail.com>
2022-01-31 00:44:57 -0500

    ocmment stuff to test options

--- scripts/build_rdkitjs.sh
+++ scripts/build_rdkitjs.sh
@@ -14,42 +14,42 @@ fi
 echo $RDKIT_DASH_VERSION
 echo $RDKIT_VERSION
 
-# Retrieve Dockerfile from main rdkit repository
-git clone https://github.com/rdkit/rdkit.git
-cd rdkit
-npm --no-git-tag-version version $RDKIT_VERSION
-git fetch --all --tags
-git checkout $RDKIT_BRANCH
-cp Code/MinimalLib/docker/Dockerfile ../Dockerfile
-cd ..
-rm -rf rdkit
+# # Retrieve Dockerfile from main rdkit repository
+# git clone https://github.com/rdkit/rdkit.git
+# cd rdkit
+# npm --no-git-tag-version version $RDKIT_VERSION
+# git fetch --all --tags
+# git checkout $RDKIT_BRANCH
+# cp Code/MinimalLib/docker/Dockerfile ../Dockerfile
+# cd ..
+# rm -rf rdkit
 
-# Clean and create distribution folder
-MINIMALLIB_OUTPUT_PATH="dist"
-rm -rf $MINIMALLIB_OUTPUT_PATH
-mkdir -p $MINIMALLIB_OUTPUT_PATH
+# # Clean and create distribution folder
+# MINIMALLIB_OUTPUT_PATH="dist"
+# rm -rf $MINIMALLIB_OUTPUT_PATH
+# mkdir -p $MINIMALLIB_OUTPUT_PATH
 
-# Build distribution files
-DOCKER_BUILDKIT=1 docker build --no-cache -f Dockerfile --build-arg RDKIT_BRANCH=$RDKIT_BRANCH -o $MINIMALLIB_OUTPUT_PATH .
-rm Dockerfile
+# # Build distribution files
+# DOCKER_BUILDKIT=1 docker build --no-cache -f Dockerfile --build-arg RDKIT_BRANCH=$RDKIT_BRANCH -o $MINIMALLIB_OUTPUT_PATH .
+# rm Dockerfile
 
-# Make dist files executable
-chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.js
-chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.wasm
+# # Make dist files executable
+# chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.js
+# chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.wasm
 
-# Move docs file in dist folder for demos to work properly
-cp docs/demo.html dist/demo.html
-cp docs/GettingStartedInJS.html dist/GettingStartedInJS.html
+# # Move docs file in dist folder for demos to work properly
+# cp docs/demo.html dist/demo.html
+# cp docs/GettingStartedInJS.html dist/GettingStartedInJS.html
 
-# Log build completed
-echo "Build completed"
-echo "MinimalLib distribution files are at $MINIMALLIB_OUTPUT_PATH"
+# # Log build completed
+# echo "Build completed"
+# echo "MinimalLib distribution files are at $MINIMALLIB_OUTPUT_PATH"
 
-# Publish
-if [ "$BETA" = "true" ]; then
-    npm publish --beta --access public
-else
-    npm publish --access public
-fi
+# # Publish
+# if [ "$BETA" = "true" ]; then
+#     npm publish --beta --access public
+# else
+#     npm publish --access public
+# fi
 
-npm run resetVersion
+# npm run resetVersion
