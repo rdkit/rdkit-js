c6e796764db69af04350ee6aaba13e58d89bea33
michelml <michmoreau.l@gmail.com>
2022-06-05 18:11:05 -0400

    test full ci with publish working

--- scripts/build_rdkitjs.sh
+++ scripts/build_rdkitjs.sh
@@ -1,51 +1,51 @@
 set -e
 
-# # Clean up temp files if exists
-# rm -rf rdkit Dockerfile
+# Clean up temp files if exists
+rm -rf rdkit Dockerfile
 
-# # Set branch to release
-# RDKIT_BRANCH="Release_$RDKIT_DASH_VERSION"
-# RDKIT_VERSION=${RDKIT_DASH_VERSION//_0/_}
-# RDKIT_VERSION=${RDKIT_VERSION//_/.}
+# Set branch to release
+RDKIT_BRANCH="Release_$RDKIT_DASH_VERSION"
+RDKIT_VERSION=${RDKIT_DASH_VERSION//_0/_}
+RDKIT_VERSION=${RDKIT_VERSION//_/.}
 
-# # make sure true/false is lowercase
-# BETA="${BETA,,}"
+# make sure true/false is lowercase
+BETA="${BETA,,}"
 
-# if [ "$BETA" = "true" ]; then
-#     RDKIT_VERSION="$RDKIT_VERSION-beta"
-# fi
+if [ "$BETA" = "true" ]; then
+    RDKIT_VERSION="$RDKIT_VERSION-beta"
+fi
 
-# echo $RDKIT_DASH_VERSION
-# echo $RDKIT_VERSION
+echo $RDKIT_DASH_VERSION
+echo $RDKIT_VERSION
 
-# # Retrieve Dockerfile from main rdkit repository
-# git clone https://github.com/michelml/rdkit.git
+# Retrieve Dockerfile from main rdkit repository
+git clone https://github.com/michelml/rdkit.git
 
-# cd rdkit
-# git fetch --all --tags
-# git checkout $RDKIT_BRANCH
-# cp Code/MinimalLib/docker/Dockerfile ../Dockerfile
-# cd ..
+cd rdkit
+git fetch --all --tags
+git checkout $RDKIT_BRANCH
+cp Code/MinimalLib/docker/Dockerfile ../Dockerfile
+cd ..
 
-# # Clean and create distribution folder
-# MINIMALLIB_OUTPUT_PATH="dist"
-# rm -rf $MINIMALLIB_OUTPUT_PATH
-# mkdir -p $MINIMALLIB_OUTPUT_PATH
+# Clean and create distribution folder
+MINIMALLIB_OUTPUT_PATH="dist"
+rm -rf $MINIMALLIB_OUTPUT_PATH
+mkdir -p $MINIMALLIB_OUTPUT_PATH
 
-# # Build distribution files
-# DOCKER_BUILDKIT=1 docker build --no-cache -f Dockerfile --build-arg RDKIT_BRANCH=$RDKIT_BRANCH -o $MINIMALLIB_OUTPUT_PATH .
+# Build distribution files
+DOCKER_BUILDKIT=1 docker build --no-cache -f Dockerfile --build-arg RDKIT_BRANCH=$RDKIT_BRANCH -o $MINIMALLIB_OUTPUT_PATH .
 
-# # Make dist files executable
-# chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.js
-# chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.wasm
+# Make dist files executable
+chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.js
+chmod a+rwx $MINIMALLIB_OUTPUT_PATH/RDKit_minimal.wasm
 
-# # Move docs file in dist folder for demos to work properly
-# cp docs/demo.html dist/demo.html
-# cp docs/GettingStartedInJS.html dist/GettingStartedInJS.html
+# Move docs file in dist folder for demos to work properly
+cp docs/demo.html dist/demo.html
+cp docs/GettingStartedInJS.html dist/GettingStartedInJS.html
 
-# # Log build completed
-# echo "Build completed"
-# echo "MinimalLib distribution files are at $MINIMALLIB_OUTPUT_PATH"
+# Log build completed
+echo "Build completed"
+echo "MinimalLib distribution files are at $MINIMALLIB_OUTPUT_PATH"
 
 # Pre-publish
 sed -i '/"private": true/d' ./package.json
