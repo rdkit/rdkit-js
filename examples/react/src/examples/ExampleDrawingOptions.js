5030c8ccc287e75e2ff579c689ca7f530e19d804
michelml <michmoreau.l@gmail.com>
2022-06-23 10:34:31 -0400

    readme edits and react demo edits

--- examples/react/src/examples/ExampleDrawingOptions.js
+++ examples/react/src/examples/ExampleDrawingOptions.js
@@ -361,7 +361,7 @@ class ExampleDrawingOptions extends React.Component {
         <div className="columns" style={{ margin: "12px 0" }}>
           <div className="column">
             <div className="field">
-              <label className="label">SVG Rendering (Default is Canvas)</label>
+              <label className="label">SVG/Canvas Rendering (Uncheck for canvas rendering)</label>
               <div className="control">
                 <input
                   className="checkbox"
