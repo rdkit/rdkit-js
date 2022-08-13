ba52ee5cf9e2a0eff9217609090cd8b4e09dbf6a
barothiadam <barothi.adam@gmail.com>
2022-08-13 23:26:47 +0200

    Add base app setup and components

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <!-- Try to stick as close to the react example design as possible -->
+  <div id="app">
+    <div id="navbar">
+      <!-- Put navbar component here -->
+      <NavBar/>
+    </div>
+    <section id="page-content" class="main-content columns is-fullheight">
+      <aside id="side-navigation" class="menu has-background-white-bis is-narrow-mobile is-fullheight section is-hidden-mobile">
+      <!-- Put side navigation component here -->
+      <SideNav/>
+      </aside>
+      <div class="container column is-10">
+        <!-- Put main components here -->
+        <Introduction/>
+        <section class="hero">
+          <div class="hero-body">
+            <p class="title" id="other-examples">Where to go next</p>
+            <p id="other-examples-vanilla-js" class="subtitle column is-two-thirds pl-0">
+              You can visit the JavaScript examples
+              <a href="https://rdkitjs.com">here</a>, which presents the
+              low-level API of RDKit.js.
+            </p>
+            <p id="other-examples-legacy" class="subtitle column is-two-thirds pl-0">
+              You can also checkout the legacy
+              <a href="https://unpkg.com/@rdkit/rdkit/dist/GettingStartedInJS.html">getting started</a>
+              and
+              <a href="https://unpkg.com/@rdkit/rdkit/dist/demo.html">demo</a>.
+            </p>
+          </div>
+        </section>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>RDKit.js</strong> by
+          <a href="https://github.com/rdkit/rdkit">Greg Landrum and the RDKit contributors</a>. The source code is licensed
+          <a href="https://github.com/rdkit/rdkit/blob/master/license.txt">BDS-3</a>.
+        </p>
+      </div>
+    </footer>
+    <a class="back_to_top" href="#navbar">&uarr;</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Introduction from './components/Introduction.vue';
+import SideNav from './components/SideNav.vue';
+import NavBar from './components/NavBar.vue';
+
+</script>
+
+<style>
+
+#page-content {
+  padding-left: 12px;
+}
+
+#side-navigation {
+  position: sticky;
+  display: inline-block;
+  max-height: 100vh;
+  top: 0;
+  box-shadow: 3px 0px 0 0 #f5f5f5;
+}
+
+.back_to_top {
+  position: fixed;
+  bottom: 80px;
+  right: 40px;
+  z-index: 9999;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  background: #f5f5f5;
+  color: #444;
+  cursor: pointer;
+  border-radius: 2px;
+  display: block;
+}
+
+.back_to_top:hover {
+  background: #e9ebec;
+}
+
+</style>
---
+++ examples/vue/src/components/NavBar.vue
@@ -0,0 +1,80 @@
+<template>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+                <img alt="logo" src="/rdkitjs_cropped_200.png"/>
+            </a>
+
+            <a role="button" :class="`navbar-burger ${burgerActiveClass}`" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="#/" @click="toggleBurger">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+    <div id="navbarBasicExample" :class="`navbar-menu ${burgerActiveClass}`">
+        <div class="navbar-start">
+            <a class="navbar-item" href="https://github.com/rdkit/rdkit-js/tree/master/examples/javascript">Source code</a>
+            <a class="navbar-item" href="https://docs.rdkitjs.com/">API Docs</a>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="#/">Examples</a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="https://rdkitjs.com">Javascript</a>
+                    <a class="navbar-item is-active" href="https://react.rdkitjs.com">React</a>
+                    <hr class="navbar-divider"/>
+                    <a class="navbar-item" href="https://github.com/rdkit/rdkit-js#contributing">Contribute</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a data-tooltip="Go to main RDKit project" class="button has-tooltip-bottom is-white is-big" href="https://github.com/rdkit/rdkit">
+                        <span class="icon">
+                            <i class="fab fa-github fa-2x"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item">
+                <div class="buttons mr-6">
+                    <a data-tooltip="Go to NPM package" class="button has-tooltip-bottom is-white is-big">
+                        <span class="icon">
+                            <i class="fab fa-npm fa-2x"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    </nav>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+let burgerActive = ref(false)
+
+let burgerActiveClass = computed(() => {
+    return burgerActive ? 'is-active' : ''
+})
+
+function toggleBurger()
+{
+    burgerActive.value = !burgerActive.value
+}
+
+</script>
+
+<style scoped>
+.navbar {
+    padding: 12px 0;
+}
+
+.navbar-brand {
+    padding: 6px;
+}
+</style>
