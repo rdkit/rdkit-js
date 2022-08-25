dbe94b216c586902dd148c897038788599d401bc
Michel Moreau <michmoreau.l@gmail.com>
2022-08-25 08:48:11 -0400

    Replace React for Vue in vue examples
    cc @adam-of-barot

--- examples/vue/src/components/Introduction.vue
+++ examples/vue/src/components/Introduction.vue
@@ -4,7 +4,7 @@
       <div class="hero-body">
         <p class="title">Introduction</p>
         <p class="subtitle column is-two-thirds pl-0">
-          Welcome to RDKit.js for React. If you didn't do it already, we first
+          Welcome to RDKit.js for Vue. If you didn't do it already, we first
           recommend to go through the
           <a href="https://rdkitjs.com#drawing-molecules-all-options">
             JavaScript examples
