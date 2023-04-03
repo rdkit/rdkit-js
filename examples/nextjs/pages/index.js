da64b0d3662c59f267b84d3a4719603ea1e8271a
Jack Zhao <jzhao@betteromics.com>
2023-04-03 13:44:28 -0700

    add nextjs example

---
+++ examples/nextjs/pages/index.js
@@ -0,0 +1,14 @@
+import dynamic from 'next/dynamic';
+const MoleculeStructure = dynamic(
+  () => import('../components/MoleculeStructure/MoleculeStructure'),
+  { ssr: false }
+);
+
+export default function Home() {
+  return (
+    <MoleculeStructure
+      structure="CSCC[C@H](NC(=O)[C@H](CC1=CNC2=C1C=CC=C2)NC(=O)CCNC(=O)OC(C)(C)C)C(=O)N[C@@H](CC(O)=O)C(=O)N[C@@H](CC1=CC=CC=C1)C(N)=O"
+      id="smiles"
+    />
+  );
+}
