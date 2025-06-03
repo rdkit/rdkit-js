7299b89e6867566a478db785dc03af9d960b7b62
Eugen <eugenbleck@gmail.com>
2025-06-03 03:49:04 -0700

    refactor: use client wrapper so that the page remains a server component

--- examples/nextjs-ts/app/page.tsx
+++ examples/nextjs-ts/app/page.tsx
@@ -1,10 +1,4 @@
-'use client'
-
-import dynamic from "next/dynamic";
-const MoleculeStructure = dynamic(
-  () => import("@/components/molecular-structure"),
-  { ssr: false }
-);
+import MoleculeStructure from "@/components/molecular-structure/client-wrapper";
 
 export default function Home() {
   return (
