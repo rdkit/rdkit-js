17583828b5f47d07f283caae1f3c0bb890eb71d9
Steffen Hirte <shirte@users.noreply.github.com>
2026-06-05 16:22:04 +1200

    Simplify nextjs example
    Disable telemetry in nextjs example. This also avoids the error message
    "npm error This command does not support workspaces." when running "npm
    run dev".
    
    Avoid copying wasm files in nextjs configuration.

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,27 +1,4 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, { isServer }) {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: "node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm",
-            to: "static/chunks"
-          }
-        ]
-      })
-    );
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false
-      };
-    }
-
-    return config;
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
