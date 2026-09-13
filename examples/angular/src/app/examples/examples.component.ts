a1eff2ea29af0af87ff8d7eafdbb2a6e432fad98
Jimmy Kromann <jimmy@charnley.dk>
2026-09-13 14:48:57 +0200

    Reset repository for new format (#682)

--- examples/angular/src/app/examples/examples.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-examples",
-  templateUrl: "./examples.component.html",
-  styleUrls: ["./examples.component.css"]
-})
-export class ExamplesComponent {}
